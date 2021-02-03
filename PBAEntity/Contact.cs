30e803f8b41b55b6328603f9716fc258ae12eb25
PidikitiVamsi <78490197+PidikitiVamsi@users.noreply.github.com>
2021-02-03 20:20:26 +0530

    Add files via upload

---
+++ PBAEntity/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBAEntity
+{
+    //Here taking Contact details
+    public class Contact
+    {
+        public int CategoryId { get; set; }
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+
+    }
+}
