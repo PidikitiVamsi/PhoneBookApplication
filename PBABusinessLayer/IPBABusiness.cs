f7b2598222ff6e808485cc703dd94c8e64da4374
PidikitiVamsi <78490197+PidikitiVamsi@users.noreply.github.com>
2021-02-03 20:17:25 +0530

    Add files via upload

---
+++ PBABusinessLayer/IPBABusiness.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBAEntity;
+
+namespace PBABusinessLayer
+{
+    public interface IPBABusiness
+    {
+       // List<Contact> GetAllcontacts();
+       // void UpdateContactById(Contact contact);
+        void AddContact(Contact contact);
+        void DeleteContactById(int id);
+       // List<Category> GetAllCategories();
+      //  List<Contact> GetAllcontacts();
+        List<Category> GetAllCategories();
+        void AddNewCategory(Category cat);
+        void DeleteCategoryById(int id);
+        List<Contact> GetAllContactsByCId(int id);
+
+    }
+}
